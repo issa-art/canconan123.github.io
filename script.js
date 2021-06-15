dc138ff067fffe7128f5fb8980e298fc65ad7133
canconan123 <minhquanlaw@yahoo.com>
2021-06-15 11:49:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ $(document).ready(function() {
     }).trigger('resize');
 
 });
+
+$('[lang]').hide(); // hide all lang attributes on start.
+$('[lang="jp"]').show(); // show just Japanese text
+$('#lang-switch').change(function () { // put onchange event when user select option from select
+    var lang = $(this).val(); // decide which language to display using switch case.
+        case 'en':
+            $('[lang]').hide();
+            $('[lang="en"]').show();
+        break;
+        case 'jp':
+            $('[lang]').hide();
+            $('[lang="jp"]').show();
+        break;
+        default:
+            $('[lang]').hide();
+            $('[lang="jp"]').show();
+        }
+});

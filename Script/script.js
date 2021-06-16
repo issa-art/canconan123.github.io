cd3920a38508d0472955daada24c232e57f93b75
canconan123 <minhquanlaw@yahoo.com>
2021-06-16 21:44:03 +0900

    Update 2 more subpages
    Announcements and FAQ

--- Script/script.js
+++ Script/script.js
@@ -41,3 +41,18 @@ $('#lang-switch').change(function () { // put onchange event when user select op
             $('[lang="jp"]').show();
         }
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
